07c9b69f3f2896c6abc0e7bdf564b8d55494678d
Risheek <101473637+RisheekShukla@users.noreply.github.com>
2024-04-09 21:31:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (67.56%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -2,22 +2,21 @@ class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) 
     {
-        int tar = tickets[k];
-        int ans=tar;
+        int ans=0;
         int afterk=0;
         for(int i=0;i<tickets.size();i++)
         {
-            if(i==k)
-            {
-                continue;
-            }
-            if(tickets[i]>=tar)
+            // if(i==k)
+            // {
+            //     continue;
+            // }
+            if(tickets[i]>=tickets[k])
             {
                 if(i>k)
                 {
                     afterk++;
                 }
-                ans += tar;
+                ans += tickets[k];
             }
             else
             {
